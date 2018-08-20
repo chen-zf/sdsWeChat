eeb7ce0d3fa9a050b13481df19d1f4ff038abd61
chen-zf <912221823@qq.com>
2018-08-20 18:07:57 +0800

    视频发布页面
    视频发布页面

--- pages/publish_video/publish_video.js
+++ pages/publish_video/publish_video.js
@@ -5,9 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    radioTypeItem:[
+      { name: 'cooking', value: '厨艺', checked: 'true'},
+      {name: 'food',value: '秀美食'},
+      {name: 'equipment',value: '设备' },
+      {name: 'brand',value: '品牌' },
+    ],
+    radioIntentItem:[
+      { name: 'show', value:'秀一秀'},
+      { name: 'work', value: '找工作' },
+    ],
+    radioEquipmentItem:[
+      { name: 'show', value: '秀设备' },
+      { name: 'sell', value: '卖设备' },
+      { name: 'benefit', value: '专利让利' },
+    ],
+    radiobrandItem: [
+      { name: 'show', value: '秀一秀' },
+      { name: 'add', value: '招商加盟' },
+    ],
+    TypeStr:'food',
+    starItem:[1,1,1,1,0]
   },
-
+  radioChange:function(e){
+    this.setData({
+      TypeStr: e.detail.value
+    })
+  },
+  starClickFunc:function(e){
+    var index = e.currentTarget.dataset.index;
+    var starArray = [0, 0, 0, 0, 0];
+    for (var z = 0; z < starArray.length; z++) {
+      if (z <= index) {
+        starArray[z] = 1;
+      }
+    }
+    this.setData({
+      starItem: starArray
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
